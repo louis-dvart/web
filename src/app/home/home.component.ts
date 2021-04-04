586c8c2eb294fac985358c3bb04121d806843b64
eximus <fabio4335@gmail.com>
2021-04-04 23:53:54 +0100

    change pics to smaller jpgs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent {
     set_bg(val: Number) : void {
         const el = document.getElementById('page-bg')
         if (el)
-            el.style.backgroundImage = "url('assets/" + val + ".png')";
+            el.style.backgroundImage = "url('assets/" + val + ".jpg')";
     }
     go_to_youtube() : void {
         window.location.href='https://www.youtube.com/channel/UCLXanXoT1vqHnqqIF8AZJqw';
