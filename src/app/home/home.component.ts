c17e9167a81939dc33a99a200d9d4ac3169cb6f1
eximus <fabio4335@gmail.com>
2021-04-04 23:22:30 +0100

    add custom fonts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,17 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit {
-    ngAfterViewInit(): void {
-        throw new Error('Method not implemented.');
-    }
-
+export class HomeComponent {
     set_bg(val: Number) : void {
         const el = document.getElementById('page-bg')
         if (el)
             el.style.backgroundImage = "url('assets/" + val + ".png')";
+    }
+    go_to_youtube() : void {
+        window.location.href='https://www.youtube.com/channel/UCLXanXoT1vqHnqqIF8AZJqw';
     }
 }
