29c5997fb7c7effb03d892429099982e03dd4d7e
eximus <fabio4335@gmail.com>
2021-04-10 23:31:14 +0100

    preload background videos and display correct one on hover

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,32 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-    private static _current_source: String = 'assets/1.mp4';
-    public static get current_source(): String {
-        return HomeComponent._current_source;
-    }
-    public static set current_source(value: String) {
-        HomeComponent._current_source = value;
-    }
+export class HomeComponent implements AfterViewInit {
+    current_video_element: number = 0;
+    video_elements: HTMLVideoElement[] = [];
 
-    @ViewChild('videoElement') videoElement!: ElementRef;
+    constructor(private elem: ElementRef){}
 
-    set_bg(val: Number) : void {
-        HomeComponent.current_source = "assets/" + val + ".mp4"
-        if (! this.videoElement.nativeElement.src.endsWith(HomeComponent.current_source)) {
-            this.videoElement.nativeElement.src = HomeComponent.current_source;
-            this.videoElement.nativeElement.load();
+    ngAfterViewInit(): void {
+        this.video_elements = this.elem.nativeElement.querySelectorAll('.videos');
+        for (const elem of this.video_elements) {
+            elem.pause();
         }
+    }
+
+    set_bg(val: number) : void {
+        if (val == this.current_video_element + 1)
+            return; // skip
+
+        this.video_elements[this.current_video_element].classList.remove('display-video');
+        this.video_elements[this.current_video_element].pause();
+        this.current_video_element = val - 1;
+        this.video_elements[this.current_video_element].classList.add('display-video');
+        this.video_elements[this.current_video_element].play();
     }
 
     go_to_youtube() : void {
