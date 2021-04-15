a39649c9b0d18aae2673828c8aa092480b4f0e7b
eximus <fabio4335@gmail.com>
2021-04-15 12:24:35 +0100

    add store component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'store', component: StoreComponent },
     { path: '**', component: HomeComponent }
 ];
 
