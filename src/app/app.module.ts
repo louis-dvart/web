a0f3422fbe5b12dd61ffe1f8b07a35f05d082cfa
eximus <fabio4335@gmail.com>
2021-04-15 12:17:58 +0100

    add bottom page icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
