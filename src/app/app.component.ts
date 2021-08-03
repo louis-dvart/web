b8a7683c9923114bcc95d4ed373dca0f90693c8d
eximus <fabio4335@gmail.com>
2021-08-03 13:42:07 +0100

    make topbar independant from bellow components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dvart-web';
+    title = 'dvart-web';
+
+    isSideMenuOpen: boolean = false;
+
+    constructor(private elem: ElementRef){}
+
+    toogleSideMenu(event : any) : void {
+        this.isSideMenuOpen = ! this.isSideMenuOpen;
+        if (this.isSideMenuOpen) {
+            this.elem.nativeElement.querySelectorAll('.side-navbar')[0].classList.add('open');
+            this.elem.nativeElement.querySelectorAll('.side-navbar-scrim')[0].classList.add('open');
+        } else {
+            this.elem.nativeElement.querySelectorAll('.side-navbar')[0].classList.remove('open')
+            this.elem.nativeElement.querySelectorAll('.side-navbar-scrim')[0].classList.remove('open')
+        }
+    }
+
+    go_to_youtube() : void {
+        window.location.href='https://www.youtube.com/watch?v=A2Keszteelg&list=PLJipLc0D7d9ReZYUE4QTzbLtfx6Kxz6yy';
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, AfterViewInit, HostListener, ElementRef} from '@angular/core';
+import { Component, AfterViewInit, HostListener} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements AfterViewInit {
     isScrollArrowShown: boolean = false;
-    isSideMenuOpen: boolean = false;
 
-    constructor(private elem: ElementRef){}
+    constructor(){}
 
     ngAfterViewInit(): void {
     }
@@ -23,21 +22,7 @@ export class HomeComponent implements AfterViewInit {
     }
 
     setScrollFlag() : void {
-         this.isScrollArrowShown = !((window.innerHeight - window.pageYOffset) / window.innerHeight < .25)
+        this.isScrollArrowShown = !((window.innerHeight - window.pageYOffset) / window.innerHeight < .25)
     }
 
-    toogleSideMenu(event : any) : void {
-        this.isSideMenuOpen = ! this.isSideMenuOpen;
-        if (this.isSideMenuOpen) {
-            this.elem.nativeElement.querySelectorAll('.side-navbar')[0].classList.add('open');
-            this.elem.nativeElement.querySelectorAll('.side-navbar-scrim')[0].classList.add('open');
-        } else {
-            this.elem.nativeElement.querySelectorAll('.side-navbar')[0].classList.remove('open')
-            this.elem.nativeElement.querySelectorAll('.side-navbar-scrim')[0].classList.remove('open')
-        }
-    }
-
-    go_to_youtube() : void {
-        window.location.href='https://www.youtube.com/watch?v=A2Keszteelg&list=PLJipLc0D7d9ReZYUE4QTzbLtfx6Kxz6yy';
-    }
 }
