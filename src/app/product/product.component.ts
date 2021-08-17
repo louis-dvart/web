a35b52b5d89586b3a7b9f77300293803ab9a757c
eximus <fabio4335@gmail.com>
2021-08-17 14:16:48 +0100

    improve popup

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,39 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Item } from '../item';
 import * as products from '../../assets/products/products.json';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+	selector: 'app-product',
+	templateUrl: './product.component.html',
+	styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-    product_id!: string;
-    product!: Item;
+	product_id!: string;
+	product!: Item;
 
-    private router: Router;
+	private router: Router;
 
-    constructor(private route: ActivatedRoute, router: Router) {
-        this.router = router;
-    }
+	constructor(private route: ActivatedRoute, router: Router) {
+		this.router = router;
+	}
 
-    public closeModal() : void {
-        this.router.navigate(
-            [ "/", { outlets: { modal: null } } ]
-        );
-    }
+	ngOnInit(): void {
+		// get route parameters
+		this.product_id = String(this.route.snapshot.paramMap.get('pid'));
+		for (let p of (products as any).default)
+			if (p.id == this.product_id)
+				this.product = p;
+		if (! this.product)
+			this.closeModal();
+	}
 
-    ngOnInit(): void {
-        // get route parameters
-        this.product_id = String(this.route.snapshot.paramMap.get('pid'));
-        for (let p of (products as any).default)
-            if (p.id == this.product_id)
-                this.product = p;
-        console.log(this.product);
+	@HostListener('document:keydown.escape', ['$event'])
+	handleEscapeKey(event: KeyboardEvent) {
+		if (event.key === "Escape") this.closeModal();
+	}
 
-        if (! this.product)
-            this.closeModal();
-    }
+	public closeModal() : void {
+		this.router.navigate(
+			[ "/", { outlets: { modal: null } } ]
+		);
+	}
+
 }
 
